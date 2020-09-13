be86e0a922bd5a7ee996723570733b24791facef
Daniel Sias <dan.sias@gmail.com>
2020-09-12 21:30:32 -0400

    Added social logins

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Socialite Google & Microsoft Login
+Route::get('/login/{provider}', [LoginController::class, 'redirectToProvider']);
+Route::get('/login/{provider}/callback', [LoginController::class, 'handleProviderCallback']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia\Inertia::render('Dashboard');

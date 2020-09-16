02c400ee94a394f8bf2e796b8524e820e4b99100
Daniel Sias <dan.sias@gmail.com>
2020-09-15 21:43:44 -0400

    User feedback and voting table

---
+++ database/migrations/2020_09_16_012304_create_feedback_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('type');
+            $table->string('url');
+            $table->text('filter');
+            $table->text('message');
+            $table->string('status');
+            $table->integer('votes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+}

dc194065dae46a97bf44c3d316d793967eccd775
Daniel Sias <dan.sias@gmail.com>
2020-09-15 21:38:20 -0400

    Index method returns partner home page

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use Illuminate\Http\Request;
+
+use App\Models\Partner;
 
 class PartnerController extends Controller
 {
-    //
+    public function index($partner = '')
+    {
+        $partners = Partner::all();
+
+        return Inertia::render('Partners', ['partner' => $partner, 'partners' => $partners]);
+    }
 }

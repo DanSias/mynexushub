17c35cb32cce1cc33a80fef908c6590d449a215d
Daniel Sias <dan.sias@gmail.com>
2020-09-15 21:43:01 -0400

    Added program requirements columns

--- database/migrations/2020_09_13_041251_create_program_requirements_table.php
+++ database/migrations/2020_09_13_041251_create_program_requirements_table.php
@@ -15,6 +15,30 @@ class CreateProgramRequirementsTable extends Migration
     {
         Schema::create('program_requirements', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('program_id')->nullable();
+            $table->string('code')->nullable();
+            $table->integer('tuition_in_state')->nullable();
+            $table->integer('tuition_out_state')->nullable();
+            $table->text('tuition_notes')->nullable();
+            $table->text('fees')->nullable();
+            $table->float('gpa_required', 5, 2)->nullable();
+            $table->text('gpa_notes')->nullable();
+            $table->string('testing_required')->nullable();
+            $table->text('testing_notes')->nullable();
+            $table->integer('tuition_credit_hour')->nullable();
+            $table->integer('tuition_credit_hour_os')->nullable();
+            $table->integer('credit_hours')->nullable();
+            $table->integer('credit_hours_max')->nullable();
+            $table->integer('program_months')->nullable();
+            $table->integer('program_months_max')->nullable();
+            $table->string('gmat')->nullable();
+            $table->string('gmat_waiver')->nullable();
+            $table->string('gre')->nullable();
+            $table->string('gre_waiver')->nullable();
+            $table->text('experience_needed')->nullable();
+            $table->string('transfer_credits_accepted')->nullable();
+            $table->text('transfer_credits_details')->nullable();
+            $table->text('program_notes')->nullable();
             $table->timestamps();
         });
     }

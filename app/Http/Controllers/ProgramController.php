50ab67bd53efca9416dab6d8b23ad10da4d947eb
Daniel Sias <dan.sias@gmail.com>
2020-09-15 21:38:44 -0400

    Index method returns program home page and single program view based on input variable

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use Illuminate\Http\Request;
+
+use App\Models\Program;
 
 class ProgramController extends Controller
 {
-    //
+    public function index($program = '')
+    {
+        if ($program == '') {
+            $programs = Program::orderBy('bu', 'asc')
+                ->orderBy('code', 'asc')
+                ->with('requirements')
+                ->get();
+        } else {
+            $programs = Program::where('code', $program)
+                ->with('requirements')
+                ->with('partner')
+                ->get();
+        }
+        return Inertia::render('Programs', ['program' => $program, 'programs' => $programs]);
+    }
 }

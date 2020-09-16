0d62cf3051eaa71570e37a5994ffa32257b493a2
Daniel Sias <dan.sias@gmail.com>
2020-09-15 21:37:53 -0400

    Added vendor css

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-]);
+mix
+    .js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+    ])
+    .styles([
+        'resources/css/multiselect.css',
+        'resources/css/tooltip.css'
+    ], 'public/css/vendor.css');

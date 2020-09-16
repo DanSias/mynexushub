02a3d54b33e715efa06a86ea9ad1b8e6823abef2
Daniel Sias <dan.sias@gmail.com>
2020-09-15 21:46:55 -0400

    Scorecard home page (research project to calculate opportunity based on degree demand, occupational outcome, and brand strength)

---
+++ resources/js/Pages/Scorecard.vue
@@ -0,0 +1,105 @@
+<template>
+    <app-layout>
+        <template #header>
+            <div class="flex items-center justify-between">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    Degree Scorecard
+                </h2>
+                <div class="flex">
+                    <SaveIcon 
+                        :selected="selected"
+                        aria-label="Save Current Scorecard" data-balloon-pos="down-right"
+                    />
+                    <LoadIcon 
+                        aria-label="Load Previous Scorecard" data-balloon-pos="down-right"
+                    />
+                </div>
+            </div>
+        </template>
+
+        <ScorecardInputs 
+            class="py-12"
+            :selected="selected"
+            @setLevel="setLevel"
+            @setCode="setCode"
+            @setOccupation="setOccupation"
+            @setSchool="setSchool"
+        />
+
+        <!-- Score Details -->
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="grid grid-cols-3 gap-4">
+                <DegreeScore 
+                    :level="selected.level"
+                    :degree="selected.degree"
+                    :school="selected.school"
+                />
+                <OccupationScore 
+                    :occupation="selected.occupation"
+                />
+                <SchoolScore 
+                    :school="selected.school"
+                />
+            </div>
+        </div>
+
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from './../Layouts/AppLayout'
+
+import SaveIcon from './Scorecard/IconSave'
+import LoadIcon from './Scorecard/IconLoad'
+
+import ScorecardInputs from './Scorecard/Inputs'
+
+import DegreeScore from './Scorecard/ScoreDegree'
+import OccupationScore from './Scorecard/ScoreOccupation'
+import SchoolScore from './Scorecard/ScoreSchool'
+
+export default {
+    name: 'Scorecard',
+
+    components: {
+        SaveIcon,
+        LoadIcon,
+        AppLayout,
+        ScorecardInputs,
+        DegreeScore,
+        OccupationScore,
+        SchoolScore
+    },
+
+    data() {
+        return {
+            selected: {
+                level: '',
+                degree: '',
+                occupation: '',
+                school: ''
+            },
+            score: {
+                degree: 0,
+                occupation: 0,
+                school: 0
+            }
+        }
+    },
+
+    methods: {
+        setLevel(level) {
+            this.selected.level = level
+        },
+        setCode(degree) {
+            this.selected.degree = degree
+        },
+        setOccupation(occupation) {
+            this.selected.occupation = occupation
+        },
+        setSchool(school) {
+            this.selected.school = school
+        }
+    },
+}
+</script>

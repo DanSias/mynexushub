13175eb284354adf0fc7cb86a84aad66f67e81ea
Daniel Sias <dan.sias@gmail.com>
2020-09-15 21:39:22 -0400

    Added methods to return all relevant data to build the scoring algorithm

--- app/Http/Controllers/ScorecardController.php
+++ app/Http/Controllers/ScorecardController.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use DB;
+use Inertia\Inertia;
 use Illuminate\Http\Request;
 
 use App\Models\Scorecard\Code;
 use App\Models\Scorecard\Occupation;
 use App\Models\Scorecard\School;
+use App\Models\Scorecard\Conferral;
 
 class ScorecardController extends Controller
 {
+    public function index()
+    {
+        return Inertia::render('Scorecard');
+    }
+
+    public function saves()
+    {
+        return \App\Models\Scorecard\Save::orderBy('updated_at', 'desc')->get();
+    }
+    
     public function codes()
     {
         return Code::all(['cip_number', 'cip6d']);
@@ -60,5 +73,75 @@ class ScorecardController extends Controller
             ->with('team')
             ->with('totalConferrals')
             ->first();
+    }
+
+    // JSON Data
+
+    public function keywords()
+    {
+        return \App\Models\Scorecard\Keyword::all();
+    }
+
+    public function conferralCodes($level = '')
+    {
+        if ($level != '') {
+            $codes = Conferral::select('cip6d', DB::raw('sum(conferrals) as conferrals'))
+                ->where('level', ucFirst($level))
+                ->orderBy('conferrals', 'desc')
+                ->groupBy('cip6d')
+                ->get();
+        } else {
+            $codes = Conferral::select('cip6d', DB::raw('sum(conferrals) as conferrals'))
+                ->orderBy('conferrals', 'desc')
+                ->groupBy('cip6d')
+                ->get();
+        }
+        // Select input expects term property
+        foreach ($codes as $code) {
+            $code->term = $code->cip6d;
+        }
+        return $codes;
+    }
+    
+    public function allCodes()
+    {
+        $codes = $this->codes();
+        // Select input expects term property
+        foreach ($codes as $code) {
+            $code->term = $code->cip6d;
+        }
+        return $codes;
+    }
+
+    
+    public function degreeOccupations($degree = '')
+    {
+        if ($degree == '') {
+            return [];
+        }
+
+        $occupations = \App\Models\Scorecard\OccupationCode::where('cip6d', $degree)
+            ->leftJoin('occupation_growth_rate', 'degree_occupations.occupation', '=', 'occupation_growth_rate.occupation')
+            ->orderBy('occupation_growth_rate.employment_2018', 'desc')
+            ->pluck('degree_occupations.occupation')
+            ->toArray();
+        return $occupations;
+    }
+
+    public function allOccupations()
+    {
+        return Occupation::all()->pluck('occupation')->toArray();
+    }
+
+    public function allSchools()
+    {
+        return School::all()->pluck('school_name')->toArray();
+    }
+
+    public function programAccreditation($code)
+    {
+        return \App\Models\Scorecard\ProgramAccreditation::where('accreditation', '!=', 'None')
+            ->where('cip6d', $code)
+            ->get(['school_name', 'level', 'cip6d', 'accreditation']);
     }
 }

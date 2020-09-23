b109def418e6e376bdca93900e1ac025e0e6c09d
Daniel Sias <dan.sias@gmail.com>
2020-09-23 11:03:21 -0400

    Test page

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container mx-auto mt-8 bg-gray-200">
+        <h2 class="text-2xl text-gray-800">Welcome to test town!</h2>
+        <TimeTabs
+           @range="setRange"
+        />
+    </div>
+</template>
+
+<script>
+import TimeTabs from '../Components/Time/Tabs'
+
+export default {
+    name: 'NexusTest',
+
+    components: {
+        TimeTabs
+    },
+
+    methods: {
+        setRange(range) {
+            console.log('testing the range for ' + range)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

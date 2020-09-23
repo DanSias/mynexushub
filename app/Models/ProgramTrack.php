7ebd636174fbef5f62f293458b56e2181e25cccf
Daniel Sias <dan.sias@gmail.com>
2020-09-23 10:54:09 -0400

    Added program relationship

--- app/Models/ProgramTrack.php
+++ app/Models/ProgramTrack.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class ProgramTrack extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function program()
+    {
+        return $this->belongsTo('App\Models\Program');
+    }
 }

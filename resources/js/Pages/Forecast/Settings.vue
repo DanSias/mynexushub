2f7673b25dd796720c712ee988780509e15eb3b1
DanSias <dan.sias@gmail.com>
2020-09-24 09:54:56 -0400

    Forecast settings form

---
+++ resources/js/Pages/Forecast/Settings.vue
@@ -0,0 +1,139 @@
+<template>
+    <div>
+        <div class="uk-width-1-5">
+            <label>Year</label>
+            <multiselect class="" 
+                v-model="form['year']"
+                :options="years" 
+                selectLabel=">" 
+                deselectLabel="x">
+            </multiselect>
+        </div>
+
+        <div class="uk-width-1-5">
+            <label>Month</label>
+            <multiselect class="" 
+                v-model="form['month']"
+                :options="months"
+                :allow-empty="false" 
+                selectLabel=">" 
+                deselectLabel="x">
+            </multiselect>
+        </div>
+
+        <div class="uk-width-1-5">
+            <label>Status</label>
+            <multiselect class="" 
+                v-model="form['status']"
+                :options="['open', 'closed']" 
+                :allow-empty="false" 
+                selectLabel=">" 
+                deselectLabel="x">
+            </multiselect>
+        </div>
+
+        <div class="uk-width-1-5">
+            <label>Note</label>
+            <input type="text" class="form-control search-box match-select-height uk-input"  v-model="form['description']" style="height: 40px;">
+        </div>
+
+        <div class="uk-width-1-5">
+            <button type="primary" class="uk-width-1-1 uk-margin-top" @click.prevent="saveSettings()">Save</button>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import Multiselect from 'vue-multiselect'
+import MonthsMixin from '../../Mixins/Months'
+
+export default {
+    name: 'ForecastSettings',
+
+    mixins: [MonthsMixin],
+
+    components: {
+        Multiselect
+    },
+
+    data() {
+        return {
+            form: {
+                year: 2020,
+                month: '',
+                status: '',
+                description: '',
+            }
+        }
+    },
+    computed: {
+        years() {
+            let d = new Date;
+            let yr = d.getFullYear();
+            let nextYear = parseInt(yr) + 1;
+            return [nextYear, yr];
+        }
+    },
+
+    watch: {
+        year() {
+            this.fillForm();
+        },
+        status() {
+            this.fillForm();
+        },
+        scenario() {
+            this.fillForm();
+        },
+        description() {
+            this.fillForm();
+        }
+    },
+
+    methods: {
+
+        fillForm() {
+            this.form.year = this.year;
+            this.form.status = this.status;
+            this.form.scenario = this.scenario;
+            this.form.description = this.description;
+        },
+
+        getForecastSettings() {
+            axios
+                .get('/forecast/settings')
+                .then(({data}) => {
+                    this.form.year = data.year;
+                    this.form.month = data.month;
+                    this.form.status = data.status;
+                    this.form.description = data.description;
+                })
+        },
+
+        saveSettings() {
+            console.log('save these settings');
+
+            axios
+                .post('/forecast/settings', { 
+                    settings: this.form 
+                })
+                .then(({data}) => {
+                    console.log(data);
+                    if (data.length > 0) {
+                        this.addMessage('Forecast Settings Saved!');
+                        this.$emit('saved');
+                    }
+                });
+        },
+
+        addMessage(msg) {
+            console.log(msg)
+        }
+    },
+
+    mounted() {
+        this.getForecastSettings();
+    }
+}
+</script>

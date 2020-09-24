8faa6d99413374b28252d09cdc6bcaf8b26cd492
Daniel Sias <dan.sias@gmail.com>
2020-09-24 10:03:37 -0400

    Added headline and components for displaying forecasting progress

--- resources/js/Pages/Forecast.vue
+++ resources/js/Pages/Forecast.vue
@@ -1,17 +1,41 @@
 <template>
     <app-layout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Marketing Forecast
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight flex justify-between">
+                <span>{{ program }} {{ channel }} Marketing Forecast</span>
+                <span class="text-gray-500 capitalize font-light">{{ month }} - December {{ year }}</span>
             </h2>
         </template>
         
-        <div class="flex">
+        <ForecastIndex 
+            v-if="program == ''"
+            class="container mx-auto"
+            :filter="filter"
+            :year="year"
+            :month="month"
+        />
 
+        <ForecastProgram 
+            v-if="program != '' && channel == ''"
+            class="container mx-auto"
+            :program="program"
+            :filter="filter"
+            :year="year"
+            :month="month"
+        />
+
+        <ForecastChannel
+            v-if="program && channel"
+            class="container mx-auto"
+            :program="program"
+            :channel="channel"
+            :filter="filter"
+            :year="year"
+            :month="month"
+            :status="status"
+        />
+        <!-- <div class="flex">
             <div id="main-content">
-                
-                <ForecastIndex />
-                
                 <div class="py-4">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
@@ -22,26 +46,32 @@
                     </div>
                 </div>
             </div>
+        </div> -->
 
-        </div>
-
-        
     </app-layout>
 </template>
 
 <script>
 import AppLayout from './../Layouts/AppLayout'
 import ForecastIndex from '../Pages/Forecast/Index'
+import ForecastProgram from '../Pages/Forecast/Program'
+import ForecastChannel from '../Pages/Forecast/Channel'
 
 export default {
     name: 'ForecastHome',
 
     components: {
         AppLayout,
-        ForecastIndex
+        ForecastIndex,
+        ForecastProgram,
+        ForecastChannel
     },
 
     props: {
+        filter: {
+            type: Object,
+            default: () => {}
+        },
         year: {
             type: Number,
             default: 0
@@ -67,7 +97,6 @@ export default {
     data() {
         return {
             filterSet: false,
-            filter: {}
         }
     },
 
@@ -78,10 +107,10 @@ export default {
     },
 
     methods: {
-        setFilter(filter) {
-            this.filter = filter
-            this.filterSet = true
-        }
+        // setFilter(filter) {
+        //     this.filter = filter
+        //     this.filterSet = true
+        // }
     },
 
     mounted() {
